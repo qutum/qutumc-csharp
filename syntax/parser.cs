0864d29583a5b416bda05a8db67f95b2baec855d
Qianyan Cai <qiantum@gmail.com>
2020-04-13 20:30:36 +0800

    new qutum grammar for block statements

--- syntax/parser.cs
+++ syntax/parser.cs
@@ -13,35 +13,39 @@ namespace qutum.syntax
 {
 	enum Syn
 	{
-		blocks, block,
-		line, parse, inner,
-		skips, skip, empty, comm
+		all = 1,
+		block, nest, stats, right, stat,
+		bin,
+		opb, opr, exp,
 	}
 
 	class Tree : Tree<Syn, Tree>
 	{
 	}
 
-	class Parsers : Parser<Lex, Syn, Tree, Lexer>
+	class Parser : Parser<Lex, Syn, Tree, Lexer>
 	{
-		static readonly string grammer =
-		@"|= EOL DED
-		blocks = block+ skips?
-		block = skips? line
-		line = parse EOL inner? =+
-		parse = BLANK* CONTENT CONTENT_BLANK* =+
-		inner = skips? IND block+ skips? DED
-		skips = skip+
-		skip = empty|comm | IND skip+ DED | DED skip+ IND
-		empty = SP* EOL =+
-		comm = SP* COMM BLANK* EOL
+		static readonly string grammar = @"
+		all   = block+ | IND all DED block*
+		block = exp stats?					=+ block
+		nest  = IND block DED				= nested block
+		stats = IND right? stat+ DED		= statements
+		right = IND stat+ DED				= statements of right datum
+		stat  = bin							= statment
+		      | EFFECT SP EOL				=| statement expected
+		bin   = opb block | opb EOL nest	=|| binary statement
+
+		opb = ADD|SUB|MUL|DIV	=+ binary operator
+		opr = EFFECT			=+ postfix operator
+		exp = EFFECT+ EOL		=+| expression
 		";
 
-		public Parsers(Lexer l) : base(grammer, l)
+		public Parser(Lexer l) : base(grammar, l)
 		{
-			treeKeep = false;
-			treeExpect = 0;
-			treeDumper = o => !(o is ArraySegment<Token<Lex>> s) ? null
+			keep = false;
+			errExpect = 0;
+			dump = 1;
+			dumper = o => !(o is ArraySegment<Token<Lex>> s) ? null
 				: s.Count == 0 ? "" : string.Join(" ", s.Select(t => t.ToString()).ToArray());
 		}
 
@@ -50,32 +54,11 @@ namespace qutum.syntax
 		public override Tree Parse()
 		{
 			var t = base.Parse();
-			if (t.head is Tree x) {
-			Loop:
-				if (x.err == 0 && x.name == Syn.empty &&
-						x.up != t && x.next?.err == 0 && x.next?.name == Syn.empty) {
-					t.Add(new Tree {
-						name = x.name, from = x.from, to = x.next.to,
-						err = 1, dump = "too many empty lines"
-					});
-					while ((x = x.next).next?.err == 0 && x.next?.name == Syn.empty)
-						;
-				}
-				if (x.head != null) {
-					x = x.head;
-					goto Loop;
-				}
-				do
-					if (x.next != null) {
-						x = x.next;
-						goto Loop;
-					}
-				while ((x = x.up) != null);
-			}
-			foreach (var k in scan.errs)
-				t.Add(new Tree {
-					name = (Syn)(-(int)k.key), from = k.from, to = k.to, err = 1, expect = k.value
-				});
+			Tree tail = t;
+			scan.errs.Each((err, x) =>
+				tail.AddNext(tail = new Tree {
+					name = (Syn)(-(int)err.key), from = ~x, to = ~x, err = -1, info = err.value
+				}));
 			return t;
 		}
 
